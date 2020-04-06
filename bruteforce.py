296cc38ddf5a74fe16f89637a9890b9959ae4076
milosmikovic <losmicarina@gmail.com>
2020-04-06 20:11:40 +0200

    dodat deo sa fjom cilja

--- bruteforce.py
+++ bruteforce.py
@@ -1,32 +1,30 @@
+import math
+
 routes = []
-routes1 = []
-def find_paths(node, cities,time, path, distance,path1,distance1):
+
+def find_paths(node, cities,time, path, distance,distance1):
 
     path.append(node)
-    path1.append(node)
 
     if len(path) > 1:
         distance += cities[path[-2]][node]
-        distance1 += time[path1[-2]][node]
+        distance1 += time[path[-2]][node]
 
     if (len(cities) == len(path)) and (path[0] in cities[path[-1]]):
         global routes
-        global routes1
         path.append(path[0])
-        path1.append(path1[0])
         distance += cities[path[-2]][path[0]]
-        distance1 += time[path1[-2]][path1[0]]
-        # print(path, distance)
-        # print(path1, distance1)
-        routes.append([distance, path])
-        routes1.append([distance1, path1])
+        distance1 += time[path[-2]][path[0]]
+        routes.append([distance,distance1, path])
         return
 
     #recursion
     for city in cities:
         if (city not in path) and (node in cities[city]):
-            find_paths(city, dict(cities), dict(time), list(path), distance, list(path1),distance1)
+            find_paths(city, dict(cities), dict(time), list(path), distance,distance1)
 
+def tezinska_fja(alfa,beta,skal_duzina,skal_vreme):
+    return alfa * skal_duzina + beta * skal_vreme
 
 if __name__ == '__main__':
     cities = {
@@ -45,35 +43,32 @@ if __name__ == '__main__':
         'E' : {'A':5,'B':5,'C':6,'D':2}
     }
 
-    find_paths('A', cities,time, [], 0, [], 0)
+    find_paths('A', cities,time, [], 0, 0)
     print("\n")
-    routes.sort()
-    routes1.sort()
-    
-    print()
-    print("Distance routes:")
-    print()
-
+    print("Velicina svih ruta je: ",len(routes))
+    print("distance/time/route")
     for i in routes:
         print(i)
     
+    sum_dist = 0
+    sum_time = 0
+    for i in routes:
+        sum_dist += i[0]
+        sum_time += i[1]
+    for i in range(len(routes)):
+        procenat_dist = routes[i][0]/sum_dist
+        procenat_time = routes[i][1]/sum_time
+        routes[i].append(procenat_dist)
+        routes[i].append(procenat_time)
+        routes[i].append(tezinska_fja(0.5,0.5,procenat_dist,procenat_time))
     print()
-    print("Time routes:")
-    print()
-
-    for i in routes1:
+    print("#####################################")
+    print("Nove rute:")
+    print("duzina/vreme/rute/norm_duzina/norm_vreme/tezinska_fja")
+    for i in sorted(routes,key=lambda route:route[5]):
         print(i)
-
-    if len(routes) != 0 and len(routes1) != 0:
-        print("Shortest distance route: %s" % routes[0])
-        print("Shortest time route: %s" % routes1[0])
-    else:
-        print("error")
-
-    best_distance_route = routes[0]
-    best_time_route = routes1[0]
-    print("Best route depending on min time/distance:")
-    if(best_distance_route <= best_time_route):
-        print(best_distance_route)
-    else:
-        print(best_time_route)
+    
+    
+    
+        
+    
